2eb471added2f1f0bb3cd38692a35dbd7aea4cf1
raphpay <raphpay.dev@gmail.com>
2023-12-04 17:48:46 +0100

    Feature: Implement login screen UI #1

---
+++ app/src/main/java/fr/twinpaw/gympulse/LoginScreen.kt
@@ -0,0 +1,79 @@
+package fr.twinpaw.gympulse
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen() {
+    var email by remember { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var passwordVisibility by remember { mutableStateOf(false) }
+
+    Box {
+        Image(
+            painter = painterResource(id = R.drawable.app_background),
+            contentDescription = stringResource(id = R.string.app_background_description),
+            contentScale = ContentScale.FillBounds,
+        )
+        
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email")},
+                textStyle = LocalTextStyle.current.copy(color = Color.Black)
+            )
+
+            PasswordTextField(
+                text = password,
+                isVisible = passwordVisibility,
+                onTextChange = { password = it },
+                changeVisibility = { passwordVisibility = !passwordVisibility }
+            )
+
+            Button(onClick = { /*TODO*/ }) {
+                Text(stringResource(R.string.log_in_text_button))
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LoginScreen()
+}
